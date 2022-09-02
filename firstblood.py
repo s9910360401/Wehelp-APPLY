5022d8a86262befdc745b012b1ece1649dec5684
s9910360401 <112683445+s9910360401@users.noreply.github.com>
2022-09-02 14:31:08 +0800

    Add files via upload

---
+++ firstblood.py
@@ -0,0 +1,13 @@
+
+from ast import If
+import decimal
+from winreg import HKEY_LOCAL_MACHINE
+
+
+print("0.0")
+print("阿豪")
+i = "阿豪"
+# ㄎ
+If (i, print("0.0"))
+jjj = "hello"
+print(jjj.lower().islower())
